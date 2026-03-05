bc52df59671af88aaea554a06da13dba665f00dc
joan-fito <joanfito9@gmail.com>
2026-03-05 19:23:55 +0000

    first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::{Context, Result};
+use log::{info, warn};
+
+#[derive(Parser)]
+struct Cli {
+    // The word we're looking for
+    #[arg(short = 'w', long)]
+    word: String,
+
+    // The path to the file to read
+    #[arg(short = 'p', long)]
+    path: std::path::PathBuf
+}
+fn main() -> Result<()> {
+    env_logger::init();
+    info!("starting");
+    let args = Cli::parse();
+
+    println!("Word: {:?}\nPath: {:?}", args.word, args.path);
+    warn!("warnign");
+    // Read the file
+    let text = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read the file `{}`", &args.path.display()))?;
+
+    // Show its content
+    for line in text.lines() {
+        if line.contains(&args.word) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
 }
